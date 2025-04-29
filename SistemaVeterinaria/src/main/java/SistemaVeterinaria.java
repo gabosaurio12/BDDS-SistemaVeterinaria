1e12f4ea404b6d635949996c9eaabda79b8f0093
Gabo-Mac <gabgonlo05@gmail.com>
2025-04-29 13:27:12 -0600

    Proyecto en netbeans

---
+++ SistemaVeterinaria/src/main/java/SistemaVeterinaria.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author gabosaurio
+ */
+public class SistemaVeterinaria {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
